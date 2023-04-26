3eb74b6c82b3af8115df31faf3276d681bc98ecb
unknown <you@example.com>
2023-04-26 17:40:08 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room-",
+      "name": "Living room ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1688,
+      "initialViewParameters": {
+        "yaw": -1.1647195111575108,
+        "pitch": 0.2786774759934154,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.042483811337142,
+          "pitch": 0.06561753470034049,
+          "rotation": 0,
+          "target": "1-bedroom-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bedroom-",
+      "name": "Bedroom ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1688,
+      "initialViewParameters": {
+        "yaw": -0.8102197891831082,
+        "pitch": 0.3884379574980006,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.300828011118865,
+          "pitch": 0.048221901528165745,
+          "rotation": 0,
+          "target": "0-living-room-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Second Floor HQ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
